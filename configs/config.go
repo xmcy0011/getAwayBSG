b04537105926985cb96c1d76af047e442299c0df
xuyc <xmcy0011@sina.com>
2019-11-13 16:10:53 +0800

    fixed

--- configs/config.go
+++ configs/config.go
@@ -19,9 +19,9 @@ func GetInstance() *singleton {
 		if config_path == "" {
 			instance = new(singleton)
 			dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-			err := config.LoadFile(dir + "/config.toml")
+			err := config.LoadFile(dir + "/config.yaml")
 			if err != nil {
-				err = config.LoadFile("./config.toml")
+				err = config.LoadFile("./config.yaml")
 				if err != nil {
 					fmt.Println("加载配置文件错误！！请确认当前目录下包含config.yaml文件或者指定配置文件参数")
 					fmt.Println(err)
