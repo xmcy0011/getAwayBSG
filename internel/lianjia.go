bc9310cafdaafe4622524c23c8abacd8bdaaf4d6
xmcy0011 <xmcy0011@sina.com>
2020-10-23 18:11:27 +0800

    fixed

--- internel/lianjia.go
+++ internel/lianjia.go
@@ -600,7 +600,7 @@ func crawlerOneDetail(startNum int, routineIndex int, houseArr []HouseInfo, tota
 
 	for i := range houseArr {
 		// sleep 100ms - 1s
-		ts := rand.Int()/900 + 100
+		ts := rand.Int()%900 + 100
 		time.Sleep(time.Millisecond * time.Duration(ts))
 		baseAttr = make([]string, 0)
 		transactionAttr = make([]string, 0)
